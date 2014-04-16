f681d99e49a2f0516b241667d1177ebf274adcaf
Arnav Gupta <championswimmer@aokp.co>
2014-04-16 13:16:16 +0530

    update docs
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

---
+++ documentation/reference/lists.js
@@ -0,0 +1,8 @@
+var DATA = [
+      { id:0, label:"in.championswimmer.sfg.lib", link:"in/championswimmer/sfg/lib/package-summary.html", type:"package" },
+      { id:1, label:"in.championswimmer.sfg.lib.GestureAnalyser", link:"in/championswimmer/sfg/lib/GestureAnalyser.html", type:"class" },
+      { id:2, label:"in.championswimmer.sfg.lib.SimpleFingerGestures", link:"in/championswimmer/sfg/lib/SimpleFingerGestures.html", type:"class" },
+      { id:3, label:"in.championswimmer.sfg.lib.SimpleFingerGestures.On1FingerGestureListener", link:"in/championswimmer/sfg/lib/SimpleFingerGestures.On1FingerGestureListener.html", type:"class" },
+      { id:4, label:"in.championswimmer.sfg.lib.SimpleFingerGestures.On2FingerGestureListener", link:"in/championswimmer/sfg/lib/SimpleFingerGestures.On2FingerGestureListener.html", type:"class" }
+
+    ];
