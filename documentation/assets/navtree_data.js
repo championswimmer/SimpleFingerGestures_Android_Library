61f3df6b52488e9a7493f8dc859c8a6cba99747e
Arnav Gupta <championswimmer@aokp.co>
2014-05-20 15:35:15 +0530

    update docs
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- documentation/assets/navtree_data.js
+++ documentation/assets/navtree_data.js
@@ -1,6 +1,6 @@
 var NAVTREE_DATA =
-[ [ "in.championswimmer.sfg.lib", "in/championswimmer/sfg/lib/package-summary.html", [ [ "Interfaces", null, [ [ "SimpleFingerGestures.On1FingerGestureListener", "in/championswimmer/sfg/lib/SimpleFingerGestures.On1FingerGestureListener.html", null, "" ], [ "SimpleFingerGestures.On2FingerGestureListener", "in/championswimmer/sfg/lib/SimpleFingerGestures.On2FingerGestureListener.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "GestureAnalyser", "in/championswimmer/sfg/lib/GestureAnalyser.html", null, "" ], [ "SimpleFingerGestures", "in/championswimmer/sfg/lib/SimpleFingerGestures.html", null, "" ] ]
+[ [ "in.championswimmer.sfg.lib", "in/championswimmer/sfg/lib/package-summary.html", [ [ "Interfaces", null, [ [ "SimpleFingerGestures.On1FingerGestureListener", "in/championswimmer/sfg/lib/SimpleFingerGestures.On1FingerGestureListener.html", null, "" ], [ "SimpleFingerGestures.OnMultiFingerGestureListener", "in/championswimmer/sfg/lib/SimpleFingerGestures.OnMultiFingerGestureListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "GestureAnalyser", "in/championswimmer/sfg/lib/GestureAnalyser.html", null, "" ], [ "GestureAnalyser.GestureType", "in/championswimmer/sfg/lib/GestureAnalyser.GestureType.html", null, "" ], [ "SimpleFingerGestures", "in/championswimmer/sfg/lib/SimpleFingerGestures.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
